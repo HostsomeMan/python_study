54305405e683b02640f6defc25493e21a303b61b
HostsomeMan <1136702931@qq.com>
2018-12-15 01:01:52 +0800

    添加sheet成功

--- common/openpyxl_python.py
+++ common/openpyxl_python.py
@@ -16,7 +16,7 @@
 '''
 
 from openpyxl import Workbook
-from openpyxl import worksheet
+from openpyxl import load_workbook
 
 import _datetime
 import time
@@ -40,25 +40,117 @@ wb = Workbook()
 
 # 写入多个单元格
 
+# ======================================================================================================================
 # 创建sheet
-ws1 = wb.create_sheet("Mysheet")        # 创建一个sheet
-ws1.title = "New Title"                 # 设定一个sheet的名字
-ws2 = wb.create_sheet("Mysheet", 0)     # 设定sheet的插入位置，默认插在后面
-ws2.title = u"你好"                       # 设定一个sheet的名字，必须是Unicode
+# ws1 = wb.create_sheet("Mysheet")        # 创建一个sheet
+# ws1.title = "New Title"                 # 设定一个sheet的名字
+# ws2 = wb.create_sheet("Mysheet", 0)     # 设定sheet的插入位置，默认插在后面
+# ws2.title = u"你好"                       # 设定一个sheet的名字，必须是Unicode
+#
+# # 获取全部sheet的名字，病例sheet名字
+# print(wb.sheetnames)
+# for sheet_name in wb.sheetnames:
+#     print(sheet_name)
+#
+# print("*"*5)
+#
+# for sheet in wb:
+#     print(sheet.title)
+#
+# # 复制一个sheet
+# wb["New Title"]["A1"] = "zeke"
+# source = wb["New Title"]
+# target = wb.copy_worksheet(source)
+#
+# wb.save("e:\\sample.xlsx")
 
-# 获取全部sheet的名字，病例sheet名字
-print(wb.sheetnames)
-for sheet_name in wb.sheetnames:
-    print(sheet_name)
+# ======================================================================================================================
 
-print("*"*5)
+# 操作单元格
+# ws1 = wb.create_sheet("Mysheet")         # 创建一个sheet
+#
+# ws1['A1'] = 1.22
+# ws1['B1'] = "你好"
+# d = ws1.cell(row=4, column=2, value=10)     # 在第四行，第二列输入值10
+#
+# print(ws1["A1"].value)
+# print(ws1["B1"].value)
+# print(d.value)
+#
+# wb.save("e:\\sample1.xlsx")
 
-for sheet in wb:
-    print(sheet.title)
+# ======================================================================================================================
 
-# 复制一个sheet
-wb["New Title"]["A1"] = "zeke"
-source = wb["New Title"]
-target = wb.copy_worksheet(source)
+# 操作批量的单元格
+# 无论ws.rows还是ws.iter_rows都是一个对象
+# 除上述两个对象外，单行、单列都是一个元祖，多行多列是二维元祖
+# ws1 = wb.create_sheet("Mysheet")
+#
+# ws1["A1"]=1
+# ws1["A2"]=2
+# ws1["A3"]=3
+#
+# ws1["B1"]=4
+# ws1["B2"]=5
+# ws1["B3"]=6
+#
+# ws1["C1"]=7
+# ws1["C2"]=8
+# ws1["C3"]=9
+#
+# # 操作单列
+# print(ws1["A"])
+# for cell in ws1["A"]:
+#     print(cell.value)
+#
+# # 操作列，获取每一个值
+# print(ws1["A:C"])
+# for cloumn in ws1["A:C"]:
+#     for cell in cloumn:
+#         print(cell.value)
+#
+# #操作多行
+# row_range = ws1[1:3]
+# print(row_range)
+# for row in row_range:
+#     for cell in row:
+#         print(cell.value)
+#
+# print("*"*50)
+# for row in ws1.iter_rows(min_row=1, min_col=1, max_col=3, max_row=3):
+#     for cell in row:
+#         print(cell.value)
+#
+# #获取所有行，按行取值
+# print(ws1.rows)
+# for row in ws1.rows:
+#     print(row)
+#     for cell in row:
+#         print(cell.value)
+#
+# print("*"*50)
+# #获取所有列，按列取值
+# print(ws1.columns)
+# for col in ws1.columns:
+#     print(col)
+#
+# wb.save("e:\\sample2.xlsx")
 
+# ======================================================================================================================
+
+# 使用百分数
+from openpyxl import load_workbook
+wb1 = load_workbook("e:\\sample.xlsx")
+wb1.guess_types = True
+ws=wb.active
+ws["D1"]="12%"
+print(ws["D1"].value)
+
+# Save the file
 wb.save("e:\\sample.xlsx")
+
+# ======================================================================================================================
+# 获取所有的行对象
+# wb1 = load_workbook("e:\\sample3.xlsx")
+# ws = wb1.active
+# rows = []
